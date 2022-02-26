226f15a772244486ebe7a9a5aad9dd55a12f30de
Luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-02-26 13:09:52 -0300

    Refactor: Diviser

--- src/components/UI/Diviser/index.tsx
+++ src/components/UI/Diviser/index.tsx
@@ -2,20 +2,21 @@ import clsx from "clsx";
 
 interface IDiviserProps {
   direction?: "vertical" | "horizontal";
+  className?: string;
 }
 
 export default function Diviser({
   direction = "horizontal",
-  ...rest
+  className,
 }: IDiviserProps) {
   const isHorizontal = direction === "horizontal";
   return (
     <div
-      {...rest}
       className={clsx({
         "bg-white opacity-5 rounded": true,
         "h-[2px] w-full": isHorizontal,
         "h-full w-[2px]": !isHorizontal,
+        [className || ""]: !!className,
       })}
     />
   );
