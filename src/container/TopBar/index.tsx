c4a50b0bb72628a064c6db80de71be0ac1044cd3
Luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-02-26 13:11:44 -0300

    Feat: top bar component

---
+++ src/container/TopBar/index.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from "react";
+import clsx from "clsx";
+
+import "./styles.css";
+
+interface ItopBarProps {
+  children: ReactNode;
+  optionBg?: 1 | 2;
+  size?: "sm" | "md" | "lg";
+  className?: string;
+}
+
+export default function TopBar({
+  optionBg = 1,
+  size = "md",
+  children,
+  className,
+  ...rest
+}: ItopBarProps) {
+  const possibleSizes = {
+    sm: "w-100",
+    md: "w-240",
+    lg: "flex-1",
+  };
+
+  const possibleBackgrounds = {
+    1: "bg-black-600",
+    2: "bg-gray-600",
+  };
+
+  return (
+    <div
+      {...rest}
+      className={clsx({
+        "topBar-container-box-shadow z-10 relative flex items-center h-48 px-8":
+          true,
+        [possibleBackgrounds[optionBg]]: optionBg,
+        [possibleSizes[size]]: size,
+        [className || ""]: className,
+      })}
+    >
+      {children}
+    </div>
+  );
+}
