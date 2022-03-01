8fed2781c251400d637f3334bbe3cddfd4463ae1
Luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-02-28 23:19:53 -0300

    Refactor: channel list in the authenticated layout

--- src/layouts/Authenticated.tsx
+++ src/layouts/Authenticated.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
+import CircleChannel from "src/components/UI/CircleChannel";
 
-import ChannelsList from "src/components/UI/ChannelsList";
+import ChannelsList from "src/container/ChannelsList";
 import mockupData from "src/mockupData";
 
 interface IAuthenticatedProps {
@@ -10,8 +11,13 @@ interface IAuthenticatedProps {
 export default function Authenticated({ children }: IAuthenticatedProps) {
   return (
     <div className="flex">
-      <ChannelsList channels={mockupData.channels} />
-      <main>{children}</main>
+      <ChannelsList>
+        <CircleChannel imgUrl="" notification={4} active />
+        {mockupData.channels.map(({ id, imgUrl, notification }) => (
+          <CircleChannel key={id} imgUrl={imgUrl} notification={notification} />
+        ))}
+      </ChannelsList>
+      <main className="flex flex-1">{children}</main>
     </div>
   );
 }
