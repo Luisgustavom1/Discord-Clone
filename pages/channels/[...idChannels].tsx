9e6f0b23b955530f9c0277c41595221a857665df
Luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-03-01 21:59:08 -0300

    Feat: friends options

--- pages/channels/[...idChannels].tsx
+++ pages/channels/[...idChannels].tsx
@@ -2,19 +2,26 @@ import IconFriends from "public/assets/svg/icon-friends.svg";
 import IconNewGroup from "public/assets/svg/icon-new-group.svg";
 import IconInbox from "public/assets/svg/icon-inbox.svg";
 import IconSupport from "public/assets/svg/icon-support.svg";
+import IconSearch from "public/assets/svg/icon-search.svg";
+import IconClose from "public/assets/svg/icon-close.svg";
+
 import Diviser from "src/components/UI/Diviser";
 import NavHeader from "src/components/UI/NavItemsHeader";
 import SideBar from "src/components/UI/SideBar";
 import TopBar from "src/container/TopBar";
 import WrappedComponent from "src/HOC/WrappedComponentAuth";
 import Authenticated from "src/layouts/Authenticated";
+import FormContext from "src/components/ReactHookForm/FormContext";
+import FriendOptions from "src/components/UI/FriendOptions";
+import { InputForm } from "src/components/ReactHookForm/InputForm";
 
 const Channels = () => {
+  const onSubmit = () => { };
   return (
     <Authenticated>
       <SideBar />
       <div className="w-full">
-        <TopBar optionBg={2} size="lg" className="px-16 py-12">
+        <TopBar optionBg="gray" size="lg" className="px-16 py-12">
           <span className="flex items-center px-8 gap-8 text-gray-400">
             <IconFriends />
             <p className="font-title text-base text-white">Amigos</p>
@@ -41,6 +48,80 @@ const Channels = () => {
             <IconSupport />
           </span>
         </TopBar>
+        <div className="flex bg-gray-600 h-auto main-content">
+          <section className="flex-1 pt-16">
+            <FormContext onSubmit={onSubmit} className="pl-32 pr-20">
+              <InputForm
+                label=""
+                name="search"
+                placeholder="Buscar"
+                className="!bg-black-800 !bg-opacity-100 mb-16"
+                Icon={IconSearch}
+                SecondIcon={IconClose}
+              />
+            </FormContext>
+            <div>
+              <h3 className="my-16 pl-32 pr-20 font-medium font-title tracking-wide text-gray-200 text-xs uppercase">
+                Online - 5
+              </h3>
+              <div>
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+                <FriendOptions />
+              </div>
+            </div>
+          </section>
+          <article className="w-384 border-x border-opacity-10 border-gray-200" />
+        </div>
       </div>
     </Authenticated>
   );
---
+++ src/components/UI/FriendOptions/index.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from "react";
+
+import IconOptions from "public/assets/svg/icon-options.svg";
+import IconChat from "public/assets/svg/icon-chat.svg";
+import AvatarUser from "../AvatarUser";
+
+const CircleIcon = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center w-36 h-36 rounded-full bg-black-600">
+      {children}
+    </div>
+  );
+};
+
+export default function FriendOptions() {
+  return (
+    <div className="border-t border-white border-opacity-5 h-60 w-508 ml-32 rounded flex items-center justify-between hover:h-[61px] hover:bg-gray-500 hover:bg-opacity-10 hover:border-0 hover:w-[524px] hover:px-8 hover:ml-24">
+      <span className="flex items-center">
+        <AvatarUser
+          userImg="/../public/assets/img/testeCanal.png"
+          userName="Luisao teste 43"
+        />
+        <div className="ml-12">
+          <p className="text-white text-base font-semibold -mb-4">
+            Gustavo Amorim
+          </p>
+          <p className="text-gray-200 text-sm font-medium">Disponível</p>
+        </div>
+      </span>
+      <span className="flex gap-12 text-gray-200">
+        <CircleIcon>
+          <IconChat />
+        </CircleIcon>
+        <CircleIcon>
+          <IconOptions />
+        </CircleIcon>
+      </span>
+    </div>
+  );
+}
